76f5f69e60ae736a9eb2008f07d6dc5be05de187
Narutzuna <gibraneduardo523@gmail.com>
2023-04-04 18:27:42 -0600

    Iniciando con JS

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+let formulario_padre = document.getElementById('formulario_padre')
+console.log(formulario_padre);
+
+
+let index = [0, 1, 2]
+
+
+let formulario = document.getElementsByClassName('contacto__nombre__input');
+formulario_padre.addEventListener('mouseenter', () => {
+    console.log(formulario);
+});
